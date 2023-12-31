e863d69f507bb76fc9a75b9aa7bdf0461733c143
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2023-12-31 13:16:10 +0100

    actualizacion

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import image_html from "./img/tailwind-html-js.png";
+import image_html from "./img/html_css_js1.png";
 import image_react from "./img/reactjs-ar21.png";
 import image_node from "./img/nodejs-ar21.png";
 import image_mongo from "./img/Mongo-db-logo.png";
@@ -7,10 +7,10 @@ import image_express from "./img/express.png";
 
 export const Curriculum = () => {
   return (
-    <section className="page">
-      <article className="education">
-        <h1 className="ancho_home">Conocimientos</h1>
-        <h1 className="ancho_home">MERN STACK</h1>
+    <div className="page">
+      <div className="education">
+        <h1>Conocimientos</h1>
+        <h1>MERN STACK</h1>
         <div>
           <img className="skills" src={image_html} alt="" />
         </div>
@@ -33,28 +33,28 @@ export const Curriculum = () => {
             Descargar CV
           </a>
         </div>
-      </article>
-      <article className="education">
+      </div>
+      <div className="education">
         <h1>Educación</h1>
-        <article className="education1">
+        <div className="education1">
           <h2>
             Certificado de Profesionalidad DESARROLLO DE APLICACIONES CON
             TECNOLOGÍAS WEB
           </h2>
           <h4>CONSORCI PER LA FORMACIÓ CONTINUA DE CATALUNYA (CONFORCAT)</h4>          
-        </article>
-        <article className="education1">
+        </div>
+        <div className="education1">
           <h2>
             Programa Especializado - Data Science Fundamentals with Python and
             SQL.
           </h2>
           <h4>Certificado a cargo de IBM en Coursera</h4>
-        </article>
-        <article className="education1">
+        </div>
+        <div className="education1">
           <h2>C2 - PROFICIENT ENGLISH CERTIFICATE (CEFR)</h2>
           <h4> EF Standard English Test (EF SET) (2023)</h4>
-        </article>
-      </article>
-    </section>
+        </div>
+      </div>
+    </div>
   );
 };
