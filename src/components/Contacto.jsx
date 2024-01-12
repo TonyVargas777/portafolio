7147c2d927a3e6471530217079c88ce57a2cee19
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2024-01-12 02:46:51 +0100

    scroll contacto

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import {React, useEffect} from "react";
 
 export const Contacto = () => {
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  
   return (
     <section className="education">
       <h2 className="heading">Contacto:</h2>
