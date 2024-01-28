01d17914564a79b90c0b19d89c2284cd64110e61
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2024-01-28 18:55:54 +0100

    css hover botones y rec headernav.jsx

---
+++ src/components/layout/HeaderNav.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import Es_Flag from "/public/spain_c.png";
+import Cat_Flag from "/public/catalonia_c.png";
+import En_Flag from "/public/usa_c.png";
+
+export const HeaderNav = () => {
+  const [t, i18n] = useTranslation("global");
+  return (
+    <header className="header">
+      <div>
+        <h1 className="port">{t("HeaderNav.portfolio")}</h1>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink
+              to="/inicio"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              {t("HeaderNav.home")}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/portafolio"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              {t("HeaderNav.portfolio")}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/curriculum"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              Curriculum
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/servicios"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              {t("HeaderNav.services")}
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contacto"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              {t("HeaderNav.contact")}
+            </NavLink>
+          </li>
+           <div>
+          
+          <button className="button_trans2" onClick={() => i18n.changeLanguage("es")}>
+            {" "}
+            <img width="24" height="24" src={Es_Flag} alt="Spanish Flag" />
+          </button>
+          <button className="button_trans2" onClick={() => i18n.changeLanguage("cat")}>
+            {" "}
+            <img width="25" height="25" src={Cat_Flag} alt="Catalan Flag" />
+          </button>
+          <button className="button_trans2" onClick={() => i18n.changeLanguage("en")}>
+            {" "}
+            <img width="24" height="24" src={En_Flag} alt="US Flag" />
+          </button>
+        </div>
+        </ul>
+      </nav>
+    </header>
+  );
+};
