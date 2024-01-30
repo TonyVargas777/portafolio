8fd7ee38118c120a7e9223bc85cd9bd3a888fc57
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2024-01-30 10:19:34 +0100

    idiomas en los proyectos

--- src/components/ListadoTrabajos.jsx
+++ src/components/ListadoTrabajos.jsx
@@ -1,28 +1,33 @@
-import {React, useEffect } from "react";
-import { trabajos } from "../data/trabajos";
+import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { trabajos } from "../data/trabajos";
 
 export const ListadoTrabajos = ({ limite }) => {
-
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const [t, i18n] = useTranslation("global");
+
   return (
     <section className="works">
       {trabajos.slice(0, limite).map((trabajo) => {
+        const nombre = trabajo[`nombre_${i18n.language}`] || trabajo.nombre;
+        const categorias = trabajo[`categorias_${i18n.language}`] || trabajo.categorias;
+
         return (
           <article key={trabajo.id} className="work-item">
             <Link to={trabajo.url} target="blank" className="work-item">
               <div className="mask">
-                <img src={"/img/" + trabajo.id + ".avif"} alt={trabajo.id} />
+                <img src={`/img/${trabajo.id}.avif`} alt={trabajo.id} />
               </div>
             </Link>
             <div className="datos">
-              <span>{trabajo.categorias}</span>
+              <span>{categorias}</span>
               <h2>
                 <Link to={trabajo.url} target="blank">
-                  {trabajo.nombre}
+                  {nombre}
                 </Link>
               </h2>
               <h3>{trabajo.tecnologias}</h3>
--- src/data/trabajos.jsx
+++ src/data/trabajos.jsx
@@ -2,37 +2,57 @@ export const trabajos = [
   {
     id: "mi_blog",
     nombre: "Mi Blog de Cine",
+    nombre_cat: "El meu Blog de Cinema",
+    nombre_en: "My Cinema Blog",
     url: "https://proyecto-modulo2-git-main-tonyvargas777.vercel.app/",
     tecnologias: "MongoDB Express React Node",
     categorias: "MERN STACK",
+    categorias_cat:"MERN STACK",
+    categorias_en:"MERN STACK"
   },
   {
     id: "juegos",
     nombre: "Los Juegos de mi Vida",
+    nombre_cat: "Els Jocs de la meva Vida",
+    nombre_en: "The Games of my Life",
     url: "https://los-juegos-de-mi-vida.onrender.com/",
     tecnologias: "REACT Node Mongo",
     categorias: "CRUD COMPLETO",
+    categorias_cat:"CRUT COMPLET",
+    categorias_en:"COMPLETE CRUD"
   },
   {
     id: "musica",
     nombre: "Compositores",
+    nombre_cat: "Compositors",
+    nombre_en: "Composers",
     url: "https://musica-maestro.vercel.app/",
     tecnologias: "HTML CSS JS REACT VITE",
     categorias: "CRUD básico con VITE",
+    categorias_cat:"CRUD bàsic amb VITE",
+    categorias_en:"Basic CRUD with VITE"
   },
   {
     id: "videojuegos",
-    nombre: "Welcome Gamers",
+    nombre: "Bienvenidos jugadores",
+    nombre_cat: "Benvinguts jugadors",
+    nombre_en: "Welcome Gamers",
     url: "https://github.com/TonyVargas777/portafolio/assets/129131613/be9b80a6-e0dc-4fdc-a07b-1fe284a4160d",
     tecnologias: "CRUD PHP",
     categorias: "Captura de Proyecto PHP",
+    categorias_cat:"Captura de Projecte PHP",
+    categorias_en:"PHP Project Capture"
   },
   {
     id: "starwars",
-    nombre: "Star Wars",
+    nombre: "Guerra de las Galaxias",
+    nombre_cat: "Guerra de les galàxies",
+    nombre_en: "Star Wars",
     url: "https://github.com/TonyVargas777/portafolio/assets/129131613/05dce3f1-3093-4552-b483-ca41fa5bcb39",
     tecnologias: "API, HTML, CSS, JS",
     categorias: "Captura API Star Wars",
+    categorias_cat:"MERN STACK",
+    categorias_en:"MERN STACK"
   }
   
 ];
