97f504393731a6c15dce858f7e8f92def14bb94e
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2024-11-29 17:18:48 +0100

    inicio flex-column translations

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -66,6 +66,7 @@ export const Inicio = () => {
           </h1>
         </div>
       </div>
+      <br />
       <h2 className="texts">{t("Inicio.text")}</h2>      
       <div className="cajas">
         <Link to="/portafolio_dS">
